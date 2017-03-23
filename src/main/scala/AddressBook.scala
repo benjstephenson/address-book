9cc0e11340866240be023a1c0d1b74325990d9be
Ben Stephenson <bjames.stephenson@gmail.com>
2017-03-23 23:33:30 +0000

    Step 2 and 3 and tests

--- src/main/scala/AddressBook.scala
+++ src/main/scala/AddressBook.scala
@@ -1,7 +1,8 @@
 import model.{ AddressEntry, Gender }
+import org.joda.time.Days
 import utils.CsvParser
 
-import scala.util.Try
+import scala.util.{ Failure, Try }
 
 object AddressBook extends AddressBook with App {
   lazy val entries = CsvParser.readFile
@@ -9,16 +10,41 @@ object AddressBook extends AddressBook with App {
 
   override def main(args: Array[String]): Unit = for {
     males <- howManyMales
+    oldest <- oldest
+    daysOlder <- daysBetweenAAndB("Bill McKnight", "Paul Robinson")
   } yield {
     println(s"Address book contains:")
     println(s" - $males males")
+    println(oldest.map(o => s" - ${o.name} is the oldest").getOrElse(" - No one is the oldest"))
+    println(s" - Bill is $daysOlder days older than Paul")
   }
 }
 
 trait AddressBook {
+
   def entries: Try[Seq[AddressEntry]]
 
   def howManyMales: Try[Int] = entries.map { e =>
     e.count(_.gender == Gender.Male)
   }
+
+  def oldest: Try[Option[AddressEntry]] = entries.map { e =>
+    e.sortWith { (a, b) =>  a.dob.isBefore(b.dob) }.headOption
+  }
+
+  def daysBetweenAAndB(a: String, b: String): Try[Int] = for {
+    entryA <- findEntry(a)
+    entryB <- findEntry(b)
+    daysBetween <- daysBetweenDob(entryA, entryB)
+  } yield daysBetween.getDays
+
+  private def findEntry(name: String): Try[AddressEntry] = entries.map { e =>
+    e.find(_.name == name).getOrElse(throw NotFound(s"No entry found for $name"))
+  }
+
+  private def daysBetweenDob(a: AddressEntry, b: AddressEntry): Try[Days] = Try {
+    Days.daysBetween(a.dob.toLocalDate, b.dob.toLocalDate)
+  }
 }
+
+case class NotFound(m: String) extends Exception(m)
--- src/test/scala/AddressBookSpec.scala
+++ src/test/scala/AddressBookSpec.scala
@@ -2,21 +2,43 @@ import model.{ AddressEntry, Gender }
 import org.joda.time.DateTime
 import org.scalatest.{ FlatSpec, Matchers }
 
-import scala.util.Success
+import scala.util.{ Failure, Success }
 
 class AddressBookSpec extends FlatSpec with Matchers {
 
   val addressBook = new AddressBook {
     override def entries = Success(List(
       AddressEntry("Charles Babbage", Gender.Male, DateTime.now.withMillisOfDay(0).withDate(1889, 12, 12)),
-      AddressEntry("Ada Lovelace", Gender.Female, DateTime.now.withMillisOfDay(0).withDate(1889, 10, 02)),
-      AddressEntry("Alan Turing", Gender.Male, DateTime.now.withMillisOfDay(0).withDate(1969, 10, 02)),
-      AddressEntry("Grace Hopper", Gender.Female, DateTime.now.withMillisOfDay(0).withDate(1999, 10, 02))
+      AddressEntry("Ada Lovelace", Gender.Female, DateTime.now.withMillisOfDay(0).withDate(1889, 10, 2)),
+      AddressEntry("Alan Turing", Gender.Male, DateTime.now.withMillisOfDay(0).withDate(1969, 10, 2)),
+      AddressEntry("Grace Hopper", Gender.Female, DateTime.now.withMillisOfDay(0).withDate(1999, 10, 2))
     ))
   }
 
   "Address book" should "find the right number of males" in {
     addressBook.howManyMales shouldBe 2
+  }
+
+  it should "find the oldest person" in {
+    addressBook.oldest shouldBe
+      Success(AddressEntry("Ada Lovelace", Gender.Female, DateTime.now.withMillisOfDay(0).withDate(1889, 10, 2)))
+  }
+
+  it should "find how many days older one person is than another" in {
+    addressBook.daysBetweenAAndB("Ada Lovelace", "Charles Babbage") shouldBe Success(71)
+  }
+
+  it should "throw an exception if the entry is not found" in {
+    addressBook.daysBetweenAAndB("someone", "someoneElse") shouldBe Failure(NotFound("No entry found for someone"))
+  }
+
+  it should "cope with an empty address book" in {
+    val addressBook = new AddressBook {
+      override def entries = Success(Nil)
+    }
+
+    addressBook.howManyMales shouldBe Success(0)
+    addressBook.oldest shouldBe Success(None)
   }
 
 }
